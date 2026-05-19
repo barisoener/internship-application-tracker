060db8bb79dd016a82ffec114f35e6a2d7905704
barisoener <barisoner155@gmail.com>
2026-05-19 17:37:44 +0200

    Add dynamic POST endpoint for applications

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+//Gelen request body JSON ise, bunu oku ve req.body içine koy.
+app.use(express.json());
+
 const PORT = 3000;
 
 const applications = [
@@ -27,17 +30,19 @@ app.get("/applications", (req, res) => {
     res.json(applications);
 });
 
+
 app.post("/applications", (req, res) => {
     const newApplication = {
-        id: 3,
-        company: "adesso",
-        position: "Frontend Intern",
-        status: "Applied"
+        id: applications.length+1,
+        company: req.body.company,
+        position: req.body.position,
+        status: req.body.status
     };
 
     applications.push(newApplication);
 
-    res.json({
+    //201 cretated
+    res.status(201).json({
         message: "Application added successfully",
         application: newApplication
     });
