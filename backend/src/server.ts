57a4f1d79f140203b3be42e5f4a84ba6c9925f0d
barisoener <barisoner155@gmail.com>
2026-06-01 17:16:09 +0200

    Implement React application creation workflow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,6 @@
+//to prevent accessing errors in the browser
+import cors from "cors";
+
 import express from "express";
 import applicationRoutes from "./routes/applicationRoutes";
 import "./config/db";
@@ -5,12 +8,18 @@ import "./config/db";
 
 const app = express();
 
+app.use(cors());
+console.log("CORS middleware loaded");
+
 app.use(express.json());
 
 app.use("/applications", applicationRoutes);
+
+
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    
 });
